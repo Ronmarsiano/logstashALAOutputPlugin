efb6f0757f38873522813d249a54322ebb8ec535
Ubuntu <romarsia@logstash-custom-plugin.d3xflvewf1xu5likmybn0acyfg.bx.internal.cloudapp.net>
2019-12-17 08:01:59 +0000

    update script

--- install_plugin.sh
+++ install_plugin.sh
@@ -1,8 +1,15 @@
 
 #! /usr/local/bin/python3
 
+echo "Remove old gem file"
 
 sudo rm logstash-output-azure_loganalytics-0.3.2.gem
+
+echo "Pulling data from github"
+
+git pull 
+
+echo "Building new logstash plugin"
 
 gem build logstash-output-azure_loganalytics.gemspec
 
@@ -10,9 +17,13 @@ cwd=$(pwd)
 
 cd /usr/share/logstash
 
+echo "Remove old plugin"
+
 sudo /usr/share/logstash/bin/logstash-plugin remove logstash-output-azure_loganalytics
 
 cd ${cwd}
+
+echo "Install new plugin"
 
 sudo /usr/share/logstash/bin/logstash-plugin install logstash-output-azure_loganalytics-0.3.2.gem
 
