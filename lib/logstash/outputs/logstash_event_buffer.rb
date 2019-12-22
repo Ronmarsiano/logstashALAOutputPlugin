e27305a5c674f12a46dd83f77726d48d8da1e051
Ronmarsiano <ronmarsiano@gmail.com>
2019-12-22 08:20:58 +0200

    dev

--- lib/logstash/outputs/logstash_event_buffer.rb
+++ lib/logstash/outputs/logstash_event_buffer.rb
@@ -1,14 +1,9 @@
 require "stud/buffer"
 
 
-class LogStash::Outputs::LogStashEventBuffer  
-    include Stud::Buffer
+class LogStash::Outputs::LogStashEventBuffer 
     def initialize(max_items, max_interval, logger)
+
         print "CCCCCCCCCCCCCCCCCCCCCRRRRRRRRRRRRRRRRRREEEEEEEEEEEAAAAAAAAAAAAAAATTTTTTTTTTTTTT"
-        buffer_initialize(
-            :max_items => @flush_items,
-            :max_interval => @flush_interval_time,
-            :logger => @logger
-          )
     end
 end
