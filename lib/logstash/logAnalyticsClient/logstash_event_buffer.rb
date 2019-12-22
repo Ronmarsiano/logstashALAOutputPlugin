4e4cda7d711c29bbaeeb0ec86cfbc59e52117b5f
Ronmarsiano <ronmarsiano@gmail.com>
2019-12-22 17:12:42 +0200

    dev

--- lib/logstash/logAnalyticsClient/logstash_event_buffer.rb
+++ lib/logstash/logAnalyticsClient/logstash_event_buffer.rb
@@ -11,13 +11,10 @@ end
 class LogStashEventBuffer 
     include Stud::Buffer
 
-    @MAX_WINDOW_SIZE = 10000
-
     def initialize(max_items, max_interval, logger,workspace_id, shared_key, endpoint, log_type,time_generated_field,flush_items)
         @log_type = log_type
         @time_generated_field = time_generated_field
         @flush_items = flush_items
-        @semaphore = Mutex.new
         @client=LogAnalyticsClient::new(workspace_id, shared_key, endpoint)
         @logger = logger
         @buffer_state = BufferState::NONE
@@ -29,23 +26,9 @@ class LogStashEventBuffer
     end
 
     public
-    def get_buffer_size()
-        return @flush_items
-    end
-
-
-    public 
     def add_event_document(event_document)
-        print "\ngoing to add event doucmnet 111111111\n\n"
-        print event_document
-        print "\ngoing to add event doucmnet 111111111\n\n"
-        buffer_receive(event_document)    
-    end
-
-    public
-    def get_buffer_status()
-        return @buffer_state
-    end 
+        buffer_receive(event_document)
+    end # def receive
 
     # called from Stud::Buffer#buffer_flush when there are events to flush
     public
@@ -60,7 +43,7 @@ class LogStashEventBuffer
         @logger.debug("Posting log batch (log count: #{documents.length}) as log type #{@log_type} to DataCollector API. First log: " + (documents[0].to_json).to_s)
         res = @client.post_data(@log_type, documents, @time_generated_field)
         if is_successfully_posted(res)
-            print_message("Message sent")
+            print "\nMessage sent\n"+ Thread.current.object_id.to_s
             @logger.debug("Successfully posted logs as log type #{@log_type} with result code #{res.code} to DataCollector API")
         else
             @logger.error("DataCollector API request failure: error code: #{res.code}, data=>" + (documents.to_json).to_s)
@@ -68,7 +51,6 @@ class LogStashEventBuffer
         rescue Exception => ex
         @logger.error("Exception occured in posting to DataCollector API: '#{ex}', data=>" + (documents.to_json).to_s)
         end
-        update_window_state(documents.length)
     end # def flush
 
     private 
@@ -76,19 +58,15 @@ class LogStashEventBuffer
       return (response.code == 200) ? true : false
     end
 
-
-
-    public 
-    def update_window_state(amount_of_documents)
-        # Reduce widow size
-        if amount_of_documents < @flush_items
-            @buffer_state = BufferState::TIME_REACHED_WINDOW_RESIZE
-        elsif @flush_items < @MAX_WINDOW_SIZE
-            @buffer_state = BufferState::FULL_WINDOW_RESIZE
-        else
-            @buffer_state = BufferState::NONE
-        end
+    public
+    def get_buffer_size()
+        return @flush_items
     end
+
+    public
+    def get_buffer_status()
+        return @buffer_state
+    end 
 
 end
 
