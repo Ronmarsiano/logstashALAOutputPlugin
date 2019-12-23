b319233c2302c6194b434a48d929ef04f71afc35
Ronmarsiano <ronmarsiano@gmail.com>
2019-12-23 11:37:48 +0200

    dev

--- lib/logstash/logAnalyticsClient/logstash_event_buffer.rb
+++ lib/logstash/logAnalyticsClient/logstash_event_buffer.rb
@@ -14,13 +14,10 @@ class LogStashEventBuffer
     include Stud::Buffer
 
     def initialize(logstash_configuration, logger)
-        @log_type = logstash_configuration.log_type
-        @time_generated_field = logstash_configuration.time_generated_field
-        @flush_items = logstash_configuration.max_items
         @client=LogAnalyticsClient::new(logstash_configuration.workspace_id, logstash_configuration.workspace_key, logstash_configuration.endpoint)
         @logger = logger
         @buffer_state = BufferState::NONE
-
+        @logstash_configuration = logstash_configuration
         buffer_initialize(
           :max_items => logstash_configuration.max_items,
           :max_interval => logstash_configuration.max_interval,
@@ -42,18 +39,18 @@ class LogStashEventBuffer
         print("\nfllusshhhiinggg\n")
         # Skip in case there are no candidate documents to deliver
         if documents.length < 1
-        @logger.debug("No documents in batch for log type #{logstash_configuration.log_type}. Skipping")
+        @logger.debug("No documents in batch for log type #{@logstash_configuration.log_type}. Skipping")
         return
         end
 
         begin
-        @logger.debug("Posting log batch (log count: #{documents.length}) as log type #{logstash_configuration.log_type} to DataCollector API. First log: " + (documents[0].to_json).to_s)
+        @logger.debug("Posting log batch (log count: #{documents.length}) as log type #{@logstash_configuration.log_type} to DataCollector API. First log: " + (documents[0].to_json).to_s)
 
-        res = @client.post_data(logstash_configuration.log_type, documents, logstash_configuration.time_generated_field)
+        res = @client.post_data(@logstash_configuration.log_type, documents, @logstash_configuration.time_generated_field)
         print "\n\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
         if is_successfully_posted(res)
             print "\nMessage sent\n"+ Thread.current.object_id.to_s
-            @logger.debug("Successfully posted logs as log type #{logstash_configuration.log_type} with result code #{res.code} to DataCollector API")
+            @logger.debug("Successfully posted logs as log type #{@logstash_configuration.log_type} with result code #{res.code} to DataCollector API")
         else
             @logger.error("DataCollector API request failure: error code: #{res.code}, data=>" + (documents.to_json).to_s)
         end
@@ -76,12 +73,12 @@ class LogStashEventBuffer
 
     public
     def get_buffer_size()
-        return logstash_configuration.flush_items
+        return @logstash_configuration.flush_items
     end
 
     public
     def get_buffer_status()
-        return logstash_configuration.buffer_state
+        return @logstash_configuration.buffer_state
     end 
 
 end
