43c216dc7ba5d7716ee73fbcb2e144b71d507333
Ronmarsiano <ronmarsiano@gmail.com>
2019-12-25 09:01:10 +0200

    debug

--- lib/logstash/outputs/azure_loganalytics.rb
+++ lib/logstash/outputs/azure_loganalytics.rb
@@ -104,7 +104,7 @@ class LogStash::Outputs::AzureLogAnalytics < LogStash::Outputs::Base
       # current_buffer = @buffers[Thread.current] != nil ?  @buffers[Thread.current] : add_buffer(Thread.current)
       # current_buffer.add_event_document2(document)
 
-      @logstash_event_buffer.add_event_document2(document)
+      @logstash_event_buffer.add_event_document(document)
 
     end
   end # def receive
