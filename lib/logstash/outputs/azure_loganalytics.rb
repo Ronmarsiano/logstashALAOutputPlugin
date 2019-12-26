44c53cc72eef0c02d93cbb54914b5037c1c41f53
Ronmarsiano <ronmarsiano@gmail.com>
2019-12-26 09:26:06 +0200

    dev

--- lib/logstash/outputs/azure_loganalytics.rb
+++ lib/logstash/outputs/azure_loganalytics.rb
@@ -53,7 +53,7 @@ class LogStash::Outputs::AzureLogAnalytics < LogStash::Outputs::Base
 
   public
   def register
-    @logstash_configuration= LogStashConfiguration::new(@workspace_id, @shared_key, @custom_log_table_name, @endpoint, @time_generated_field, @key_names, @key_types, @plugin_flush_interval)
+    @logstash_configuration= LogStashConfiguration::new(@workspace_id, @workspace_key, @custom_log_table_name, @endpoint, @time_generated_field, @key_names, @key_types, @plugin_flush_interval)
     validate_configuration()
     # Initialize the logstash resizable buffer
     # This buffer will increase and decrease size according to the amount of messages inserted.
