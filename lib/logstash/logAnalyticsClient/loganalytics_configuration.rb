48e0c5fd71f73c8abea76f04c416e9cc9d02341a
Ronmarsiano <ronmarsiano@gmail.com>
2019-12-29 10:22:30 +0200

    dev

--- lib/logstash/logAnalyticsClient/loganalytics_configuration.rb
+++ lib/logstash/logAnalyticsClient/loganalytics_configuration.rb
@@ -11,7 +11,7 @@ class LogStashConfiguration
         @plugin_flush_interval = plugin_flush_interval
         @MAX_WINDOW_SIZE = 60000
         @MIN_WINDOW_SIZE = 1    
-        @max_items = 50
+        @max_items = 5000
         @increase_factor =increase_factor
         @logger = logger    
     end
