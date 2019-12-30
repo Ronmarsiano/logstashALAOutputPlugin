fe8c6faeef42637f74815d3c19764c9a03ac6ea1
Ronmarsiano <ronmarsiano@gmail.com>
2019-12-30 09:53:44 +0200

    dev

--- lib/logstash/logAnalyticsClient/logStashAutoResizeBuffer.rb
+++ lib/logstash/logAnalyticsClient/logStashAutoResizeBuffer.rb
@@ -70,6 +70,11 @@ class LogStashAutoResizeBuffer
 
     private
     def change_max_size(amount_of_documents, documents_byte_size)
+        print (documents_byte_size / amount_of_documents)
+        print (amount_of_documents == @logstash_configuration.max_items)
+        print ((2 * @logstash_configuration.max_items) * average_document_size) < @logstash_configuration.MAX_SIZE_BYTES)
+        print( @logstash_configuration.MAX_SIZE_BYTES / average_document_size)
+
         average_document_size = documents_byte_size / amount_of_documents
 
         # If window is full we need to increase it 
