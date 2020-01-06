0eceda31ef24ee583f53f02c2f8dc4831674170e
Ronmarsiano <ronmarsiano@gmail.com>
2020-01-06 09:59:41 +0200

    dev

--- lib/logstash/logAnalyticsClient/logStashAutoResizeBuffer.rb
+++ lib/logstash/logAnalyticsClient/logStashAutoResizeBuffer.rb
@@ -59,6 +59,7 @@ class LogStashAutoResizeBuffer
             end
             rescue Exception => ex
                 @logger.error("Exception in posting data to Azure Loganalytics.\n[Exception: '#{ex}'\nDocuments(#{amount_of_documents}) failed to be sent.[documents= '#{documents_json}']")
+                resend_message(documents_json, amount_of_documents)
             end
     end # end send_message_to_loganalytics
 
