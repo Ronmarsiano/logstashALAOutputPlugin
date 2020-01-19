6de8e1f0aff8788b2d24f24afeace8cb783209a9
Ronmarsiano <ronmarsiano@gmail.com>
2020-01-19 13:15:12 +0200

    dev

--- lib/logstash/logAnalyticsClient/logAnalyticsClient.rb
+++ lib/logstash/logAnalyticsClient/logAnalyticsClient.rb
@@ -58,7 +58,7 @@ class LogAnalyticsClient
   # Return the current data 
   def rfc1123date()
     current_time = Time.now
-    current_time.httpdate()
+    return current_time.httpdate()
     # return current_time
   end # def rfc1123date
 
