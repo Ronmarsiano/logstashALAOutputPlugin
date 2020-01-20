0fefc73257b20d08de54983d5f343295a92bdc1d
Ronmarsiano <ronmarsiano@gmail.com>
2020-01-20 07:43:12 +0200

    dev

--- install_plugin.sh
+++ install_plugin.sh
@@ -1,10 +1,37 @@
 
 #! /usr/local/bin/python3
 
+echo Install Java
+
+sudo apt-get update
+
+sudo apt install openjdk-11-jre-headless
+
+echo Download and install the Public Signing Key
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+
+echo Save the repository definition to /etc/apt/sources.list.d/elastic-7.x.list
+
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+
+echo "update"
+
+sudo apt-get update
+
+echo "install logstash"
+
+sudo apt-get install logstash
+
 echo "Try to update\\install ruby"
 
 sudo apt  install ruby
 
+echo "Starting logstash service in 3 seconds"
+
+sleep 3
+
+sudo systemctl start logstash.service
 
 echo "Remove old gem file"
 
